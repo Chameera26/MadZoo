e4b38ca35f8fc4deef2e6ffb1f7242c016433a3f
Chameera26 <Shathisalahakoon@gmail.com>
2025-05-24 09:36:23 +0530

    I add new MadZoo

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 function toggleMenu() {
   const nav = document.getElementById('navLinks');
   nav.classList.toggle('show');
-
-  const dropdowns = document.querySelectorAll('.dropdown');
-  dropdowns.forEach(drop => {
-    drop.addEventListener('click', () => {
-      drop.classList.toggle('open');
-    });
-  });
 }
